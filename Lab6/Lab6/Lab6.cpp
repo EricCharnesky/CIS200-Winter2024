206004a7ad32dad2b453046ca3fc1dfd3a988c8e
Eric Charnesky <echarnes@umich.edu>
2024-02-20 16:22:24 -0500

    printing board

---
+++ Lab6/Lab6/Lab6.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printBoard(vector<vector<char>>& const board) {
+    cout << "-----------------" << endl;
+    for (vector<char> row : board) {
+        for (char spot : row) {
+            cout << "|" << spot;
+        }
+        cout << "|" << endl;
+        cout << "-----------------" << endl;
+    }
+}
+
+int main()
+{
+    vector<vector<char>> board = {
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
+    };
+
+    printBoard(board);
+}
